bb55b636f24b56b4386ae21d75697fcf95c7e824
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-10-08 10:02:39 +0530

    handle logout issue failed

--- boiler-plate/src/app/api/auth/logout.js
+++ boiler-plate/src/app/api/auth/logout.js
@@ -1,3 +1,14 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleLogout } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default async function logout(req, res) {
+  try {
+    await handleLogout(req, res, {
+      returnTo: process.env.AUTH0_BASE_URL, // Where to redirect after logout
+      logoutParams: {
+        federated: true,  // This ensures logging out from Auth0 SSO as well
+      },
+    });
+  } catch (error) {
+    res.status(error.status || 400).end(error.message);
+  }
+}
